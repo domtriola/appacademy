6598506536b4e7cc7c3c5190e85c70d0891dc584
Dominick Triola <dominick@ignovadesign.com>
2016-11-19 21:48:49 -0800

    fill in structure to pass specs

--- homework/w1d5/simon/lib/simon.rb
+++ homework/w1d5/simon/lib/simon.rb
@@ -4,19 +4,31 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +36,7 @@ class Simon
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +48,12 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Simon.new().play
 end
