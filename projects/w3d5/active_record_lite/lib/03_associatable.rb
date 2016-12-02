e10c47c78e9376b19889787d349d271edc4c7b02
Dominick Triola <dominick@ignovadesign.com>
2016-12-02 14:59:14 -0800

    finish Associatable

--- projects/w3d5/active_record_lite/lib/03_associatable.rb
+++ projects/w3d5/active_record_lite/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -10,34 +11,50 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.singularize.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] ||
+                   (name.to_s.singularize + '_id').to_sym
+    @class_name = options[:class_name] ||
+                  name.to_s.singularize.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] ||
+                   (self_class_name.to_s.downcase.singularize + '_id').to_sym
+    @class_name = options[:class_name] ||
+                  name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key = options.send(:foreign_key)
+      options.model_class.where(id: self.send(foreign_key)).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self, options)
+    define_method(name) do
+      foreign_key = options.send(:foreign_key)
+      options.model_class.where(foreign_key => self.id)
+    end
   end
 
   def assoc_options
@@ -46,5 +63,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
