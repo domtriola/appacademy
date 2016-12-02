62eb1136a203650425ee58ba83137986d4654b33
Dominick Triola <dominick@ignovadesign.com>
2016-12-02 11:38:56 -0800

    create class methods for SQLObject

--- projects/w3d5/active_record_lite/lib/01_sql_object.rb
+++ projects/w3d5/active_record_lite/lib/01_sql_object.rb
@@ -5,42 +5,84 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    unless @cols
+      @cols = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{table_name}
+      SQL
+
+      @cols = @cols[0].map(&:to_sym)
+    end
+
+    @cols
   end
 
   def self.finalize!
+    columns.each do |col|
+      define_method(col) do
+        attributes[col]
+      end
+
+      define_method("#{col}=") do |val|
+        attributes[col] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |hash| self.new(hash) }
   end
 
   def self.find(id)
-    # ...
+    obj = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+
+    obj.empty? ? nil : self.new(obj[0])
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      self.send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+
   end
 
   def insert
