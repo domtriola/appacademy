60ab1cb8f75f71198bc3601ae2276a7caa88c645
Dominick Triola <dominick@ignovadesign.com>
2016-12-05 14:14:03 -0800

    rename to ContactsAPI

---
+++ projects/w4d1/ContactsAPI/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Short-hand
+  # resources :users
+
+  # Long-hand
+  get    'users/'    => 'users#index'
+  post   'users/'    => 'users#create'
+  get    'user/new'  => 'users#new',  as: 'new_user'
+  get    'user/edit' => 'users#edit', as: 'edit_user'
+  get    'users/:id' => 'users#show', as: 'user'
+  patch  'users/:id' => 'users#update'
+  put    'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+end
