d8528f7577e48628568f508668e45c0eefd3c7d6
Dominick Triola <dominick@ignovadesign.com>
2016-12-08 14:54:26 -0800

    create bands controller and views

---
+++ projects/w4d4/full_rails/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,30 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
+
+  def new
+  end
+
+  def create
+    band = Band.new(band_params)
+    if band.save
+      redirect_to band_url(band)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @band = Band.find_by(id: params[:id])
+  end
+
+  def edit
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
