c44f1850c73359bcc3f12e1fbce3d9921d3370bd
Dominick Triola <dominick@ignovadesign.com>
2016-12-08 17:33:30 -0800

    create functionality to add tracks and albums

--- projects/w4d4/full_rails/MusicApp/app/controllers/albums_controller.rb
+++ projects/w4d4/full_rails/MusicApp/app/controllers/albums_controller.rb
@@ -14,11 +14,11 @@ class AlbumsController < ApplicationController
 
   def create
     album = Album.new(album_params)
-    if album.save
-      redirect_to album_url(album)
+    if album.save!
+      redirect_to band_url(album.band)
     else
       flash[:errors] = album.errors.full_messages
-      render :new
+      # render :new
     end
   end
 
--- projects/w4d4/full_rails/MusicApp/app/controllers/tracks_controller.rb
+++ projects/w4d4/full_rails/MusicApp/app/controllers/tracks_controller.rb
@@ -1,2 +1,25 @@
 class TracksController < ApplicationController
+  def show
+    @track = Track.find_by(id: params[:id])
+  end
+
+  def new
+    @album = Album.find_by(id: params[:album_id])
+  end
+
+  def create
+    track = Track.new(track_params)
+    if track.save!
+      redirect_to album_url(track.album)
+    else
+      flash[:errors] = track.errors.full_messages
+      # render :new
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :album_id, :status, :lyrics)
+  end
 end
