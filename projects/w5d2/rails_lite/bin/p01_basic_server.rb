597209138a7cd001cd14f088af1fa9f410530459
Dominick Triola <dominick@ignovadesign.com>
2016-12-13 13:09:10 -0800

    create basic rack server and app

--- projects/w5d2/rails_lite/bin/p01_basic_server.rb
+++ projects/w5d2/rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,23 @@
 require 'rack'
+
+hello_app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write("Hello world!")
+  res.finish
+end
+
+display_path = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  path = req.path
+  res.write(path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: display_path,
+  Port: 3000
+)
