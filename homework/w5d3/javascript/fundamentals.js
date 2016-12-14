434df12e9cf9aa96fdf81d7233a7e0922898cfc0
Dominick Triola <dominick@ignovadesign.com>
2016-12-13 22:25:14 -0800

    create simple functions

--- homework/w5d3/javascript/fundamentals.js
+++ homework/w5d3/javascript/fundamentals.js
@@ -2,66 +2,88 @@
 **********/
 
 
-// var is function scope
-function mysteryScoping1() {
-  var x = 'out of block';
-  if (true) {
-    var x = 'in block';
-    console.log(x);
-  }
-  console.log(x);
-}
-mysteryScoping1();
-// in block
-// in block
+// // var is function scope
+// function mysteryScoping1() {
+//   var x = 'out of block';
+//   if (true) {
+//     var x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// mysteryScoping1();
+// // in block
+// // in block
+//
+// // const is block scope
+// function mysteryScoping2() {
+//   const x = 'out of block';
+//   if (true) {
+//     const x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// mysteryScoping2();
+// // in block
+// // out of block
+//
+// // var tries to redeclare constant
+// function mysteryScoping3() {
+//   const x = 'out of block';
+//   if (true) {
+//     var x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// mysteryScoping3();
+// // SyntaxError: Identifier 'x' has already been declared
+//
+// // let is block scope
+// function mysteryScoping4() {
+//   let x = 'out of block';
+//   if (true) {
+//     let x = 'in block';
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// mysteryScoping4();
+// // in block
+// // out of block
+//
+// // attempting to redeclare x with let in same scope
+// function mysteryScoping5() {
+//   let x = 'out of block';
+//   if (true) {
+//     let x = 'in block';
+//     console.log(x);
+//   }
+//   let x = 'out of block again';
+//   console.log(x);
+// }
+// mysteryScoping5();
+// // SyntaxError: Identifier 'x' has already been declared
 
-// const is block scope
-function mysteryScoping2() {
-  const x = 'out of block';
-  if (true) {
-    const x = 'in block';
-    console.log(x);
-  }
-  console.log(x);
-}
-mysteryScoping2();
-// in block
-// out of block
 
-// var tries to redeclare constant
-function mysteryScoping3() {
-  const x = 'out of block';
-  if (true) {
-    var x = 'in block';
-    console.log(x);
-  }
-  console.log(x);
-}
-mysteryScoping3();
-// SyntaxError: Identifier 'x' has already been declared
 
-// let is block scope
-function mysteryScoping4() {
-  let x = 'out of block';
-  if (true) {
-    let x = 'in block';
-    console.log(x);
-  }
-  console.log(x);
-}
-mysteryScoping4();
-// in block
-// out of block
 
-// attempting to redeclare x with let in same scope
-function mysteryScoping5() {
-  let x = 'out of block';
-  if (true) {
-    let x = 'in block';
-    console.log(x);
-  }
-  let x = 'out of block again';
-  console.log(x);
-}
-mysteryScoping5();
-// SyntaxError: Identifier 'x' has already been declared
+/* Functions
+*************/
+
+const madLib = (verb, adjective, noun) => {
+  console.log(`We shall ${verb.toUpperCase()} the ` +
+              `${adjective.toUpperCase()} ${noun.toUpperCase()}.`);
+};
+// madLib("make", "best", "guac");
+// We shall MAKE the BEST GUAC.
+
+
+const isSubstring = (string, sub) => {
+  return string.indexOf(sub) > -1;
+};
+// console.log(isSubstring("time to program", "time"));
+// // true
+// console.log(isSubstring("Jump for joy", "joys"));
+// // false
