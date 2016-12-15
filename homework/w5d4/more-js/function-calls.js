dbf12a7a5eaef923df79318c3f3e9f863f3921a7
Dominick Triola <dominick@ignovadesign.com>
2016-12-14 21:47:11 -0800

    add function call examples

---
+++ homework/w5d4/more-js/function-calls.js
@@ -0,0 +1,23 @@
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`);
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
+
+Noodles.chase(Markov);
+// My name is Noodles and I'm chasing Markov! Woof!
+Noodles.chase.call(Markov, Noodles);
+// My name is Markov and I'm chasing Noodles! Woof!
+Noodles.chase.apply(Markov, [Noodles]);
+// My name is Markov and I'm chasing Noodles! Woof!
