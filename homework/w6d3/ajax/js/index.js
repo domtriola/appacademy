8c52a27e34da701b70d6fb146a83ce0aefc3db95
Dominick Triola <dominick@ignovadesign.com>
2016-12-20 13:30:52 -0800

    add ajax request as callback to evaluate different times actions happen

--- homework/w6d3/ajax/js/index.js
+++ homework/w6d3/ajax/js/index.js
@@ -1,5 +1,23 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+let req = () => {
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    type: "GET",
+    success(response) {
+      console.log(response);
+      console.log("Request returned: ", Date.now());
+    }
+  });
+};
+
+function sendRequest(request) {
+  console.log("Sending request: ", Date.now());
+  request();
+}
+
+sendRequest(req);
 
 // Add another console log here, outside your AJAX request
+console.log("Log from after ajax request declared: ", Date.now());
