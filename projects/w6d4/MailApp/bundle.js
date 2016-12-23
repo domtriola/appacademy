b432835fb102acf073d2c29f5333f97f6e6a9a11
Dominick Triola <dominick@ignovadesign.com>
2016-12-22 16:38:14 -0800

    configure menu items to change location hash on click

--- projects/w6d4/MailApp/bundle.js
+++ projects/w6d4/MailApp/bundle.js
@@ -44,7 +44,15 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	
+	document.addEventListener("DOMContentLoaded", function() {
+	  const sidebarItems = document.querySelectorAll(".sidebar-nav li");
+	  sidebarItems.forEach(item => {
+	    item.addEventListener("click", (e) => {
+	      let innerText = e.currentTarget.innerText.toLowerCase();
+	      window.location.hash = innerText;
+	    });
+	  });
+	});
 
 
 /***/ }
--- projects/w6d4/MailApp/main.js
+++ projects/w6d4/MailApp/main.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+  const sidebarItems = document.querySelectorAll(".sidebar-nav li");
+  sidebarItems.forEach(item => {
+    item.addEventListener("click", (e) => {
+      let innerText = e.currentTarget.innerText.toLowerCase();
+      window.location.hash = innerText;
+    });
+  });
+});
