eb08be49361e2bedb33446b689af567ce5e3904c
Dominick Triola <dominick@ignovadesign.com>
2016-12-24 12:24:18 -0800

    create reducer

--- homework/w7d1/redux/DollarStore/frontend/reducer.js
+++ homework/w7d1/redux/DollarStore/frontend/reducer.js
@@ -1,10 +1,21 @@
+import merge from 'lodash/merge';
+
 const initialState = {
   baseCurrency: "Please select",
   rates: {}
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  Object.freeze(state);
+  switch(action.type) {
+    case "SWITCH_CURRENCY":
+      const nextState = merge({}, state);
+      nextState.rates = action.rates;
+      nextState.baseCurrency = action.baseCurrency;
+      return nextState;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
