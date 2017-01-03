cbf5e18d19bff3b2c1b059d2e888d4978d4cada0
Dominick Triola <dominick@ignovadesign.com>
2017-01-02 22:47:25 -0800

    refactor to use applyMiddleware

--- homework/w7d2/redux_middleware/TodoApp/frontend/store/store.js
+++ homework/w7d2/redux_middleware/TodoApp/frontend/store/store.js
@@ -1,23 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
-// const addLoggingToDispatch = store => next => action => {
-//   console.log("Action receive: ", action);
-//   console.log("State pre-dispatch: ", store.getState());
-//
-//   let result = next(action);
-//
-//   console.log("State post-dispatch: ", store.getState());
-//
-//   return result;
-// };
+const addLoggingToDispatch = store => next => action => {
+  console.log("Action receive: ", action);
+  console.log("State pre-dispatch: ", store.getState());
+
+  let result = next(action);
+
+  console.log("State post-dispatch: ", store.getState());
+
+  return result;
+};
 
 const configureStore = (preloadedState = {}) => {
-  // const store = createStore(RootReducer,
-  //                           preloadedState,
-  //                           applyMiddleware(addLoggingToDispatch));
   const store = createStore(RootReducer,
-                            preloadedState);
+                            preloadedState,
+                            applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
--- homework/w7d2/redux_middleware/TodoApp/frontend/todo_redux.jsx
+++ homework/w7d2/redux_middleware/TodoApp/frontend/todo_redux.jsx
@@ -5,23 +5,10 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const addLoggingToDispatch = store => {
-    const dispatch = store.dispatch;
-    return action => {
-      console.log(store.getState());
-      console.log(action);
-
-      dispatch(action);
-
-      console.log(store.getState());
-    };
-  };
-
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const initStore = configureStore(preloadedState);
-  initStore.dispatch = addLoggingToDispatch(initStore);
+  const store = configureStore(preloadedState);
 
   const root = document.getElementById('content');
-  ReactDOM.render(<Root store={initStore} />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
