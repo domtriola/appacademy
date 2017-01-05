17eb2b5d658384cddc15dbd22521cfe66a57d665
Dominick Triola <dominick@ignovadesign.com>
2017-01-04 17:35:04 -0800

    add routes and click handlers for color components

--- homework/w7d4/react-router/rainbow_routes/entry.jsx
+++ homework/w7d4/react-router/rainbow_routes/entry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory, withRouter } from 'react-router';
+import { Router, Route, IndexRoute,
+         hashHistory, withRouter } from 'react-router';
 
 import Red from './components/red.jsx';
 import Orange from './components/orange.jsx';
@@ -38,27 +39,35 @@ class Rainbow extends React.Component {
   }
 
   addRed() {
-    // your code here
+    this.props.router.push('/red');
   }
 
   addGreen() {
-    // your code here
+    this.props.router.push('/green');
   }
 
   addBlue() {
-    // your code here
+    this.props.router.push('/blue');
   }
 
   addViolet() {
-    // your code here
+    this.props.router.push('/violet');
   }
-};
+}
 
 Rainbow = withRouter(Rainbow);
 
 const routes = (
   <Route path="/" component={Rainbow}>
-    // your routes here
+    <Route path="red" component={Red}>
+      <Route path="orange" component={Orange} />
+      <Route path="yellow" component={Yellow} />
+    </Route>
+    <Route path="green" component={Green} />
+    <Route path="blue" component={Blue}>
+      <Route path="indigo" component={Indigo} />
+    </Route>
+    <Route path="violet" component={Violet} />
   </Route>
 );
 
