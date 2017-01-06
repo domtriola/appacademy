f069ae5a3989422c208b1e1afe342d545de5126f
Dominick Triola <dominick@ignovadesign.com>
2017-01-06 13:02:52 -0800

    setup user and session controllers

--- projects/w7d5/frontend-auth/BenchBnB/app/controllers/api/sessions_controller.rb
+++ projects/w7d5/frontend-auth/BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,20 @@
 class Api::SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(user_params[:username],
+                                    user_params[:password])
+    if user
+      render json: { user: user.username, id: user.id }
+    else
+      render json: { errors: "Invalid username or password", status: 401 }
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: { status: 404 }
+    end
+  end
 end
--- projects/w7d5/frontend-auth/BenchBnB/app/controllers/application_controller.rb
+++ projects/w7d5/frontend-auth/BenchBnB/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_methods :current_user, :logged_in?
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
   def login_user!(user)
     @current_user = user
     session[:session_token] = user.reset_session_token!
-    redirect_to root_url
   end
 
   def ensure_logged_in
