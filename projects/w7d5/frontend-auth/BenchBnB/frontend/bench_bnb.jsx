15c5f88c0bf31947a06279d6f8979b8112642fbf
Dominick Triola <dominick@ignovadesign.com>
2017-01-08 18:36:59 -0800

    setup session form

--- projects/w7d5/frontend-auth/BenchBnB/frontend/bench_bnb.jsx
+++ projects/w7d5/frontend-auth/BenchBnB/frontend/bench_bnb.jsx
@@ -5,6 +5,12 @@ import Root from './components/root.jsx';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
+  // TODO: testing
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// TODO: testing
+import { login } from './actions/session_actions';
+window.login = login;
