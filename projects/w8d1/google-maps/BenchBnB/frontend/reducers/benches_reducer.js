b38b3741df118e59e7fee05c6f00ea0d4d4f0f78
Dominick Triola <dominick@ignovadesign.com>
2017-01-09 16:37:11 -0800

    create benches reducer

---
+++ projects/w8d1/google-maps/BenchBnB/frontend/reducers/benches_reducer.js
@@ -0,0 +1,11 @@
+import { RECEIVE_BENCHES } from '../actions/bench_actions';
+
+const benchesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_BENCHES:
+      return action.benches;
+    default:
+      return state;
+  }
+};
